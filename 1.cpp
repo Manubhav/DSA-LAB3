849dafe8c53bc33682c301b95b168a71590555c7
Manubhav <54354723+Manubhav@users.noreply.github.com>
2019-10-17 14:24:18 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+    int main()
+    {
+      int i, j, n, s;
+      printf("Enter the number of rows:\n");
+      scanf("%d", &n);
+      s=n;
+      for (i=1;i<=n;i++)  // Loop to print rows
+      {
+        for (j=1;j<s;j++)  // Loop to print spaces in a row
+          	{
+			printf(" ");
+		}
+        s--;
+        for (j=1;j<=2*i-1;j++) // Loop to print stars in a row
+         	{
+		  	printf("*");
+    	}
+	 	printf("\n");
+      }
+      return 0;
+    }
+
